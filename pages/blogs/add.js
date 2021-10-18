a268d37283625547353119dfd74539163c47dd22
Winton <wintondvllrs2@gmail.com>
2021-10-18 14:44:05 +0200

    added ability to add blog post(no image yet) added toastify for error messages on form

--- pages/blogs/add.js
+++ pages/blogs/add.js
@@ -5,6 +5,8 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { API_URL } from '../../config/index'
 import styles from '../../styles/Form.module.css'
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function AddBlogsPage() {
     const [values, setValues]= useState({
@@ -17,9 +19,29 @@ export default function AddBlogsPage() {
     })
 
     const router =useRouter ()
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(values)
+        // validation
+        const hasEmptyFields = Object.values(values).some((element) => element === '' )
+        if(hasEmptyFields){
+            toast.error('Please fill in all fields')
+        }
+
+        const res = await fetch(`${API_URL}/blogs`, {
+            method: 'POST',
+            headers: {
+                'Contenet-Type': 'application/json'
+            },
+            body: JSON.stringify(values)
+        })
+
+        if(!res.ok){
+            toast.error('Something went wrong')
+        }else {
+            const evt = await res.json()
+            router.push(`/events/${evt.slug}`)
+        }
+        // console.log(values)
     }
 
     const handleInputChange = (e) => {
@@ -32,6 +54,7 @@ export default function AddBlogsPage() {
             <Link href='/blogs'>Go Back</Link>
             <h1>Add Events</h1>
 
+            <ToastContainer />
             <form onSubmit={handleSubmit} className={styles.form} >
                 <div className={styles.grid}>
                     <div>
